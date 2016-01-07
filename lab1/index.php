b6ec49f3624b13eb359d0559013c1cd26bf69641
Richard <Richard@172.16.28.1>
2016-01-07 14:47:09 -0800

    stories - responsive

--- lab1/index.php
+++ lab1/index.php
@@ -11,16 +11,15 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        echo 'Hello World!';
-        $temp = 'Jim';
-        echo 'Hi, my name is';
-        echo $temp;
-        $temp = 'geek';
-        echo 'I am a';
-        echo $temp;
-        $temp = 10;
-        echo 'My level is';
-        echo $temp;
+        $name = 'Jim';
+        $what = 'geek';
+        $level = 10;
+        echo 'Hi, my name is '.$name,'. and I am a level '.$level.' '.$what;
+        
+        $hoursworked = 10;
+        $rate = 12;
+        $total = $hoursworked * $rate;
+        echo 'You owe me '.$total;
         ?>
     </body>
 </html>
