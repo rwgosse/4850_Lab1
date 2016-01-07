e70b0ef6bd26cde18910bbc9ef558a0061e69075
Richard <Richard@172.16.28.1>
2016-01-07 15:21:37 -0800

    stories - responsive

--- lab1/index.php
+++ lab1/index.php
@@ -15,11 +15,48 @@ and open the template in the editor.
         $what = 'geek';
         $level = 10;
         echo 'Hi, my name is '.$name,'. and I am a level '.$level.' '.$what;
+        echo '<br>';
         
-        $hoursworked = 10;
+        if (isset($_GET['hours'])) {
+            $hoursworked = $_GET['hours'];
+        } else {
+            $hoursworked = 10;
+        }
         $rate = 12;
-        $total = $hoursworked * $rate;
-        echo 'You owe me '.$total;
+   
+        
+        if ($hoursworked > 40) {
+            $total = $hoursworked * $rate * 1.5;
+        } else {
+            $total = $hoursworked * $rate;
+        }
+        echo ($total > 0) ? 'You owe me '.$total : "You're welcome";
+        
+        if (isset($GET_['board'])){
+            $position = $_GET['board'];
+            $squares = str_split($position);
+            if (winner('x',$squares)) echo 'You Win!';
+            else if (winner('o',$squares)) echo 'I Win!';
+            else echo 'No Winner Yet';
+        }
+        
+        
         ?>
     </body>
 </html>
+
+<script>
+function winner ($token,$position) {
+            $won = false;
+            if (($position[0] == $token) &&
+                    ($position[1] == $token) &&
+                    ($position[2] == $token)) {
+                $won = true;
+                    } else if (($position[3] == $token) &&
+                            ($position[4] == $token) &&
+                            ($position[5] == $token)) {
+                        
+            }
+            return $won;
+        }
+        </script>
