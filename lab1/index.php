28fe4fb671dee8b8db4167fb96ce35f5b71cdbab
Richard <Richard@172.16.28.1>
2016-01-07 18:53:00 -0800

    stories - responsive

--- lab1/index.php
+++ lab1/index.php
@@ -11,52 +11,85 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+        function winner($token, $position) {
+        $won = false;
+        if (($position[0] == $token) &&
+        ($position[1] == $token) &&
+        ($position[2] == $token)) {
+        $won = true;
+        } else if (($position[3] == $token) &&
+        ($position[4] == $token) &&
+        ($position[5] == $token)) {
+        $won = true;
+        } else if (($position[6] == $token) &&
+        ($position[7] == $token) &&
+        ($position[8] == $token)) {
+        $won = true;
+        } else if (($position[0] == $token) &&
+        ($position[3] == $token) &&
+        ($position[6] == $token)) {
+        $won = true;
+        } else if (($position[1] == $token) &&
+        ($position[4] == $token) &&
+        ($position[7] == $token)) {
+        $won = true;
+        } else if (($position[2] == $token) &&
+        ($position[5] == $token) &&
+        ($position[8] == $token)) {
+        $won = true;
+        } else if (($position[0] == $token) &&
+        ($position[4] == $token) &&
+        ($position[8] == $token)) {
+        $won = true;
+        } else if (($position[2] == $token) &&
+        ($position[4] == $token) &&
+        ($position[6] == $token)) {
+        $won = true;
+        }
+        return $won;
+        }
+
         $name = 'Jim';
         $what = 'geek';
         $level = 10;
-        echo 'Hi, my name is '.$name,'. and I am a level '.$level.' '.$what;
+        echo 'Hi, my name is ' . $name, '. and I am a level ' . $level . ' ' . $what;
         echo '<br>';
-        
+
         if (isset($_GET['hours'])) {
-            $hoursworked = $_GET['hours'];
+        $hoursworked = $_GET['hours'];
         } else {
-            $hoursworked = 10;
+        echo 'You did not specify # of hours, assuming 10';
+        echo '<br>';
+        $hoursworked = 10;
         }
         $rate = 12;
-   
-        
+
+
         if ($hoursworked > 40) {
-            $total = $hoursworked * $rate * 1.5;
+        $total = $hoursworked * $rate * 1.5;
         } else {
-            $total = $hoursworked * $rate;
+        $total = $hoursworked * $rate;
         }
-        echo ($total > 0) ? 'You owe me '.$total : "You're welcome";
-        
-        if (isset($GET_['board'])){
-            $position = $_GET['board'];
-            $squares = str_split($position);
-            if (winner('x',$squares)) echo 'You Win!';
-            else if (winner('o',$squares)) echo 'I Win!';
-            else echo 'No Winner Yet';
+        echo ($total > 0) ? 'You owe me $ ' . $total : "You're welcome";
+
+        if (isset($_GET['board'])) {
+        echo '<br>board set';
+        echo '<br>';
+        $position = $_GET['board'];
+        $squares = str_split($position);
+        if (winner('x', $squares)) {
+        echo 'You Win!';
+        } else if (winner('o', $squares)) {
+        echo 'I Win!';
+        } else {
+        echo 'No Winner Yet';
         }
-        
-        
+        } else {
+        echo '<br> Board is not set';
+        }
         ?>
     </body>
+
 </html>
 
-<script>
-function winner ($token,$position) {
-            $won = false;
-            if (($position[0] == $token) &&
-                    ($position[1] == $token) &&
-                    ($position[2] == $token)) {
-                $won = true;
-                    } else if (($position[3] == $token) &&
-                            ($position[4] == $token) &&
-                            ($position[5] == $token)) {
-                        
-            }
-            return $won;
-        }
-        </script>
+
